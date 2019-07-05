0dbb8a8d54cf8b54001781f4e8955a62818e5554
rktel <rakinakuy@hotmail.com>
2019-07-05 17:31:49 -0500

    avl SOCKS 2

--- server/main.js
+++ server/main.js
@@ -1,12 +1,12 @@
 //VARIABLES
 
-SOCKS_0 
-SOCKS_1
-SOCKS_2
-SOCKS_3
-SOCKS_4
+SOCKS_0 = []
+SOCKS_1 = []
+SOCKS_2 = []
+SOCKS_3 = []
+SOCKS_4 = []
 SOCKS_5 = ["Pepa"]
-SOCKS_6
-SOCKS_7
-SOCKS_8
-SOCKS_9
+SOCKS_6 = []
+SOCKS_7 = []
+SOCKS_8 = []
+SOCKS_9 = []
